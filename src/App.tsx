2deb54f9f4b685998c9b5f7713adf2cffeb8ed03
abriham <abrihamatinkut@gmail.com>
2026-03-07 12:05:29 +0300

    test node version is correct for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Experience } from '@/components/Experience';
 // import { Projects } from '@/components/Projects';
 import { Contact } from '@/components/Contact';
 import { Footer } from '@/components/Footer';
-
+  
 export default function Component() {
   return (
     <div className="min-h-screen bg-background">
